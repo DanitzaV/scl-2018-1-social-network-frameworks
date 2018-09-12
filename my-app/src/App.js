dbef6468800eb37e7796494418f29ee6057e41ec
valecj <valeriacossio5@gmail.com>
2018-09-12 08:25:21 -0300

    f

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+//import './App.css';
+import Title from '../src/Componentes/Registro/Title'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Title titulo = "Crea tu cuenta"/>
+      </div>
+    );
+  }
+}
+
+export default App;

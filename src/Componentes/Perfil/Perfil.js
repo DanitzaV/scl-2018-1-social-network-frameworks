a85994b04eb8ce9b639733f667a6086cc796ac99
viviana <dmpviviana@gmail.com>
2018-09-28 01:53:39 -0300

    arreglo perfil

--- src/Componentes/Perfil/Perfil.js
+++ src/Componentes/Perfil/Perfil.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import firebase from '../../base';
-import Button from '@material-ui/core/Button';
-import Modal from './Modal';
-import { withStyles } from '@material-ui/core/styles';
-import { Grid, FormControl, Typography } from '@material-ui/core';
 
-const styles = theme => ({
-  
-    paper: {
-      padding: theme.spacing.unit * 2,
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-    input: {
-      display: 'none',
-    },
-  });
+import app from '../../base';
+import { Paper, Grid, Button, Avatar, Input, Divider, Typography } from '@material-ui/core';
+import IconButton  from '@material-ui/core/IconButton';
+import  PhotoCamera  from '@material-ui/icons/PhotoCamera';
+import Edit from '@material-ui/icons/Edit'
+import AddIcon from '@material-ui/icons/Add';
+import Icon from '@material-ui/core/Icon';
+import './Perfil.css'
+
+
+
 
 class Perfil extends Component {
     constructor(props) {
@@ -23,10 +18,7 @@ class Perfil extends Component {
 
         this.state = {
             email: '',
-            
-            name: '',
-            phone: ''
-        } 
+        };
     }
 
     componentWillMount() {
@@ -39,35 +31,60 @@ class Perfil extends Component {
 
     render() {
         return (
-            <div>
-
-
-               <FormControl margin="normal" fullWidth>
-                <Typography variant="subheading">Imagen (opcional)</Typography>
-                <input
-                accept="image/*"
-              
-                id="contained-button-file"
-                multiple
-                type="file"
-                onChange={this.handleOnChange}
-              />
-                <label htmlFor="contained-button-file">
-                  <Button variant="contained" component="span">
-                    Upload
-                  </Button>
-                </label>
-              </FormControl>
-
-                <Modal />
-
-                <h1>Email: {this.state.email}</h1>
-
-                <span>nombre: {this.props.nombre}</span>
+            <Grid container  style={{ padding: 20 }}>
+                <Grid container justify="center">
+                <Grid item>
+                    <Avatar aria-label="Recipe" className='bigAvatar'>{this.state.email.charAt(0).toUpperCase()}</Avatar>
+                    <Grid item>
+                       
+                    </Grid>
+                </Grid>
+                </Grid>
                
-                <span>celular: {this.props.fono}</span>
+
+                <Grid container spacing={18} justify="center" style={{ paddingTop: 20, paddingBottom: 20 }}>
+                   <Grid item>
+                   <Typography align="center" variant="headline">{this.state.email}</Typography>
+                   </Grid>
+
+                    <Grid item className="formEdit">
+                        <Input placeholder="Escribe aqui" value={this.state.value} onChange={this.handleChange} fullWidth />
+                    </Grid>
+                    <Grid item className="formEdit" >
+                        <input accept="image/*" className="btnNone" id="icon-button-file" type="file" onChange={this.handleOnChange} />
+                        <label htmlFor="icon-button-file">
+                            <IconButton color="primary" component="span">
+                                <PhotoCamera ali />
+                            </IconButton>
+                        </label>
+                        <Button variant="fab" color="secondary" aria-label="Edit"  >
+                            <Edit></Edit>
+                        </Button>
+                    </Grid>
+
+                </Grid>
                 
-            </div>
+                <Grid item xs={12} md={12} xl={12}>
+                    <Divider />
+                </Grid>
+                <Grid container justify="center" >
+                <Grid item  >
+                <Button color="primary">Editar Perfil</Button>
+
+                </Grid>
+                </Grid>
+                <Grid container >
+                <Grid item xs={12} md={12} xl={12} >
+                <Typography  variant="Body1">Phone: </Typography>
+                <Typography  variant="Body1">Sobre Mi: </Typography>
+
+                </Grid>
+                </Grid>
+                
+
+
+
+            </Grid>
         )
     }
 };
